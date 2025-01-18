c37d18797584ccb65c917a78759ad53b2bdb0f50
ralfy <ralfy@RALFY>
2025-01-18 19:23:17 -0300

    creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registrotecnico.Models;
+using System.Collections.Generic;
+
+namespace Registrotecnico.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+              : base(options) { }
+        public DbSet<Tecnicos> Tecnico { get; set; }
+    }
+}
