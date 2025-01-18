2fd971e6a12cdaad155376672e43c8cabf56dbc6
ralfy <ralfy@RALFY>
2025-01-18 19:24:43 -0300

    creando el modelo de tecnico

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registrotecnico.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio ")]
+        public string? Nombres { get; set; }
+
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio")]
+        public decimal? Sueldohora { get; set; }
+    }
+}
