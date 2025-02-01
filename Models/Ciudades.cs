c9f86a5bb69f6bb8a1e860921c2ca3f19f1a978d
ralfy <ralfy@RALFY>
2025-02-01 15:53:16 -0300

    creando el modelo de ciudades

---
+++ Models/Ciudades.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registrotecnico.Models
+{
+    public class Ciudades
+    {
+        [Key]
+        public int CiudadId { get; set; }
+
+        public string Nombres { get; set; }
+    }
+}
