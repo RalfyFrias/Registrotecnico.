c9e31a98ddc1e72e80c6a9d626d94e68a06a62e9
ralfy <ralfy@RALFY>
2025-02-08 16:05:31 -0300

    crenado el modelo de sistemas

---
+++ Models/Sistemas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registrotecnico.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio ")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio ")]
+        public string Complejidad { get; set; }
+    }
+}
